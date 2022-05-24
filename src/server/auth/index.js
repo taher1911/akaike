c461056493ae7065657471007fc3ae32ca83fd03
taher1911 <72612843+taher1911@users.noreply.github.com>
2022-05-24 21:33:34 +0200

    Add files via upload

--- src/server/auth/index.js
+++ src/server/auth/index.js
@@ -1,7 +1,7 @@
 import Api from "../index";
 
-export function server_login(data) {
-  return Api().post(`/api/login`, data);
+export function server_register(data) {
+  return Api().post(`user/register`, data);
 }
 
 export function server_logout() {
